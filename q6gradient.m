d6293c27012d1400d52fb6ae8529867d38c0739c
Zongyi Liu <112992888+zongyiliu@users.noreply.github.com>
2023-11-28 11:07:31 -0500

    Update q6gradient.m

--- q6gradient.m
+++ q6gradient.m
@@ -1,20 +1,39 @@
+randn('state',1);
+m=200;
+n=100;
 ALPHA = 0.01;
 BETA = 0.5;
 MAXITERS = 1000;
+NTTOL = 1e-8;
 GRADTOL = 1e-3;
+% generate random problem
+A = randn(m,n);
+% gradient method
+vals = []; steps = [];
 x = zeros(n,1);
 for iter = 1:MAXITERS
-   val = -sum(log(1-A*x)) - sum(log(1+x)) - sum(log(1-x));
-   grad =  A’*(1./(1-A*x)) - 1./(1+x) + 1./(1-x);
-   if norm(grad) < GRADTOL, break; end;
-   v = -grad;
-       fprime = grad’*v;
-       t = 1;  while ((max(A*(x+t*v)) >= 1) | (max(abs(x+t*v)) >= 1)),
-          t = BETA*t;
-       end;
-       while ( -sum(log(1-A*(x+t*v))) - sum(log(1-(x+t*v).^2)) >  ...
-               val + ALPHA*t*fprime )
-          t = BETA*t;
-       end;
-       x = x+t*v;
-    end;
+val = -sum(log(1-A*x)) - sum(log(1+x)) - sum(log(1-x));
+vals = [vals, val];
+d = 1./(1-A*x);
+grad = A'*d - 1./(1+x) + 1./(1-x);
+v = -grad;
+fprime = grad'*v;
+norm(grad)
+if norm(grad) < GRADTOL, break; end;
+t = 1;
+while ((max(A*(x+t*v)) >= 1) | (max(abs(x+t*v)) >= 1)),
+t = BETA*t;
+end;
+while ( -sum(log(1-A*(x+t*v))) - sum(log(1-(x+t*v).^2)) > ...
+val + ALPHA*t*fprime )
+t = BETA*t;
+end;
+x = x+t*v;
+steps = [steps,t];
+end;
+figure(1)
+semilogy([0:(length(vals)-2)], vals(1:length(vals)-1)-optval, '-');
+xlabel('x'); ylabel('z');
+figure(2)
+plot([1:length(steps)], steps, ':',[1:length(steps)], steps, 'o');
+xlabel('x'); ylabel('z');
